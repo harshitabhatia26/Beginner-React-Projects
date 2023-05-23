94c148b7a272992843ac528f1c6aaab356262237
harshitabhatia26 <bharshita2604@gmail.com>
2023-05-23 20:08:18 +0530

    Added files

---
+++ quote-gen/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+
+const displayQuote = event => alert(event.target.id);
+const quotes = [
+  {
+    name:"Quote-1",
+    text: "How far that little candle throws its beams! So shines a good deed in a naughty world."
+  },
+  {
+    name:"Quote-2",
+    text: "All is flux, nothing stays still."
+  },
+  {
+    name:"Quote-3",
+    text: "Intuition will tell the thinking mind where to look next."
+  }
+];
+
+function App() {
+  const greeting = "greeting"; //variable called greeting with the value greeting
+  //aria-label is for screen readers
+  return (
+    <div className="main">
+   <h1 id={greeting}> Quote Generator</h1>
+   <p>Quotes to change your life.</p>
+   <ul>
+  {    
+    quotes.map(quote => (
+      <li key={quote.name}>
+        <button onClick={displayQuote}>
+          <span aria-label={quote.name} id={quote.text}>
+            {quote.name}
+            </span>
+        </button>
+      </li>
+    )
+    )
+  }
+   </ul>
+   </div>
+  )
+}
+
+export default App;
