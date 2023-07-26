53c5920f61b8def0c4d20a9411b40918739e95c2
harshitabhatia26 <bharshita2604@gmail.com>
2023-07-27 02:19:56 +0530

    Image functionality added

--- meme-generator/src/form.js
+++ meme-generator/src/form.js
@@ -5,22 +5,21 @@ function Form() {
     const [memeImage, setMemeImage] = useState("");
     function getMeme() {
         const memesArray = memesdata.data.memes;
-        const randomNumber = Math.floor(Math.random * memesArray.length);
-        setMemeImage(memesArray[randomNumber].url);
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        setMemeImage( () => memesArray[randomNumber].url)
         console.log("hey")
     }
     return (
-        <div>
-            <form className="d-flex mx-auto my-5 justify-content-center">
+            <div className="d-flex mx-auto my-5 justify-content-center">
                 <div className="form-group d-flex flex-column d-md-block">
                     <input type="text" placeholder="Enter heading text" className="mr-3 my-3">
                     </input>
                     <input type="text" placeholder="Enter bottom text" className=" my-3">
                     </input>
                     <button type="submit" onClick={getMeme} className="btn btn-primary btn-lg btn-block mx-auto my-1.5 font-weight-bold">Get a new meme image</button>
+                    <img className="meme my-4" src={memeImage} />
                 </div>
-                <img src={memeImage} />
-            </form>
+
         </div>
     )
 }
