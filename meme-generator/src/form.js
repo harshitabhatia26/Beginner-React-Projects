1cf51e875789dde5adc9adbab71f6eac7a3bd8aa
harshitabhatia26 <bharshita2604@gmail.com>
2023-07-29 23:13:54 +0530

    Form functioning added and API linked

--- meme-generator/src/form.js
+++ meme-generator/src/form.js
@@ -1,23 +1,53 @@
-import memesdata from "./memesdata";
+
 import { useState } from "react";
+import { useEffect } from "react";
 
 function Form() {
-    const [memeImage, setMemeImage] = useState("");
+    const[meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = useState([])
+    
+    useEffect(() => {
+            fetch("https://api.imgflip.com/get_memes")
+                .then(res=> res.json())
+                .then(data => setAllMemeImages(data.data.memes))
+    }, [])
+
     function getMeme() {
-        const memesArray = memesdata.data.memes;
-        const randomNumber = Math.floor(Math.random() * memesArray.length);
-        setMemeImage( () => memesArray[randomNumber].url)
-        console.log("hey")
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length);
+        const url = allMemeImages[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
+
+    function handleChange(event) {
+        setMeme(prevState => {
+            return {
+                ...prevState,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
     return (
-            <div className="d-flex mx-auto my-5 justify-content-center">
+            <div className="d-flex mx-auto my-5 justify-content-center align-items-center">
                 <div className="form-group d-flex flex-column d-md-block">
-                    <input type="text" placeholder="Enter heading text" className="mr-3 my-3">
+                    <input type="text" placeholder="Enter heading text" onChange={handleChange} name="topText" value={meme.topText} className="toptext my-3">
                     </input>
-                    <input type="text" placeholder="Enter bottom text" className=" my-3">
+                    <input type="text" placeholder="Enter bottom text" onChange={handleChange} name="bottomText" value={meme.bottomText} className="my-3">
                     </input>
                     <button type="submit" onClick={getMeme} className="btn btn-primary btn-lg btn-block mx-auto my-1.5 font-weight-bold">Get a new meme image</button>
-                    <img className="meme my-4" src={memeImage} />
+                    <div className="meme">
+                        <img className="meme--image my-4" src={meme.randomImage} />
+                        <h2 className="meme--text top">{meme.topText}</h2>
+                        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                    </div>
+
                 </div>
 
         </div>
